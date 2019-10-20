daca6f7155eccd3a37d7246b56416f57d107284c
Pankajan Satkunam <pankajan05@gmail.com>
2019-10-20 06:41:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyApplication
+ This is angular kotlin app. this is my first app. I create it for learn the angular mobile development
---
+++ app/src/main/java/com/kajan/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kajan.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class   MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnlogin.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+    }
+}
